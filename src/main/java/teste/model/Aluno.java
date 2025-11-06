8f027cad154edb83ea88c791ccb2e610e138c1f7
miguel1606-creator <miguel.marangoni@aluno.ifsp.edu.br>
2025-11-06 19:59:26 -0300

    Refactor Aluno class structure and fields
    Refactor Aluno class by updating field types and adding new fields.

--- src/main/java/teste/model/Aluno.java
+++ src/main/java/teste/model/Aluno.java
@@ -1,13 +1,11 @@
-package teste.model;
-
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.ManyToOne;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -27,14 +25,21 @@ public class Aluno implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EqualsAndHashCode.Include
-	@ToString.Include
+	@ToString.Include	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;	
-	private String nome;
+
 	private String prontuario;
-	private float salario;
-	private Date datanascimento;
+	private String nome;
+	private boolean ativo;
+
 	private String telefone;
-	private Boolean ativo;
+	private Float salario;
+	private Date dataNascimento;
+
+
+	@ManyToOne
+	private Professor professor;
+	
 }
